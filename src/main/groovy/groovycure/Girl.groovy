164e4f3e4126c5aedc9530c1e88129850b6588ec
ligun <ligun.net@gmail.com>
2014-07-16 15:27:08 +0900

    クラス変数の型を明示的に指定

--- src/main/groovy/groovycure/Girl.groovy
+++ src/main/groovy/groovycure/Girl.groovy
@@ -1,16 +1,23 @@
 class Girl { 
-  private def name = "";
-  private def precure_name = "";
-  private def transformed = false
+  private String name = ''
+  private String  precure_name = ''
+  private boolean transformed = false
+  private String prologue = ''
 
-  Girl(name, precure_name) {
+  Girl(name, precure_name, prologue) {
     this.name = name
     this.precure_name = precure_name
+    this.prologue = prologue
   }
 
   @Override
   String toString() {
     return "${this.name}(${this.precure_name})"
+  }
+
+  def transform(){
+    this.transformed = true
+    return this.prologue
   }
 
   String getName(){
--- src/test/groovy/groovycure/GirlsSpec.groovy
+++ src/test/groovy/groovycure/GirlsSpec.groovy
@@ -1,10 +1,13 @@
 import spock.lang.*
 
 class GirlsSpec extends Specification {
-  def "変身前の名前テスト"() {
-    setup:
-    def sut = new Girl("りぐん", "キュアニート")
+  def sut
 
+  def setup(){
+    sut = new Girl("りぐん", "キュアニート", "あふれる汗！キュアニート！")
+  }
+
+  def "変身前の名前テスト"() {
     when:
     sut.transformed = false
 
@@ -13,13 +16,18 @@ class GirlsSpec extends Specification {
   }
 
   def "変身後の名前テスト"() {
-    setup:
-    def sut = new Girl("りぐん", "キュアニート")
-    
     when:
     sut.transformed = true
 
     then:
     sut.name == "キュアニート"
+  }
+
+  def "名前のテスト"() {
+    when:
+    String name = sut
+    
+    then:
+    name == "りぐん(キュアニート)"
   }
 }
