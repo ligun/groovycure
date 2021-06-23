1ad6807e2ecc251ed6210daefb18fe6f8f66430a
ligun <choonggeun@gmail.com>
2021-06-24 01:09:54 +0900

    Migrate the configuration to ConfigSlurper

---
+++ src/main/resources/PrecureConfig.groovy
@@ -0,0 +1,484 @@
+girls {
+    nagisa {
+        name = '美墨なぎさ'
+        precure_name = 'キュアブラック'
+        prologue = '''\
+光の使者、キュアブラック
+光の使者、キュアホワイト
+ふたりはプリキュア！
+闇の力のしもべたちよ！
+とっととお家に帰りなさい！\
+'''
+    }
+    honoka {
+        name = '雪城ほのか'
+        precure_name = 'キュアホワイト'
+        prologue = '''\
+光の使者、キュアブラック
+光の使者、キュアホワイト
+ふたりはプリキュア！
+闇の力のしもべたちよ！
+とっととお家に帰りなさい！\
+'''
+    }
+    hikari {
+        name = '九条ひかり'
+        precure_name = 'シャイニールミナス'
+        prologue = '輝く命！シャイニールミナス！\n光の心と光の意志、全てをひとつにするために！\n'
+    }
+    saki {
+        name = '日向咲'
+        precure_name = 'キュアブルーム'
+        prologue = '''\
+はばたけ大地に！
+輝く金の花！キュアブルーム！
+聖なる泉を汚すものよ！
+阿漕な真似はおやめなさい！\
+'''
+    }
+    mai {
+        name = '美翔舞'
+        precure_name = 'キュアイーグレット'
+        prologue = '''\
+はばたけ空に！
+煌めく銀の翼！キュアイーグレット！
+聖なる泉を汚すものよ！
+阿漕な真似はおやめなさい！\
+'''
+    }
+    nozomi {
+        name = '夢空のぞみ'
+        precure_name = 'キュアドリーム'
+        prologue = '大いなる希望の力、キュアドリーム！'
+    }
+    rin {
+        name = '夏木りん'
+        precure_name = 'キュアルージュ'
+        prologue = '情熱の赤い炎、キュアルージュ！'
+    }
+    urara {
+        name = '春日野うらら'
+        precure_name = 'キュアレモネード'
+        prologue = 'はじけるレモンの香り、キュアレモネード！'
+    }
+    komachi {
+        name = '秋元こまち'
+        precure_name = 'キュアミント'
+        prologue = 'やすらぎの緑の大地、キュアミント！'
+    }
+    karen {
+        name = '水無月かれん'
+        precure_name = 'キュアアクア'
+        prologue = '知性の青き泉、キュアアクア！'
+
+    }
+    kurumi {
+        name = '美々野くるみ'
+        precure_name = 'ミルキィローズ'
+        prologue = '青いバラは秘密のしるし！ミルキィローズ！'
+    }
+    love {
+        name = '桃園ラブ'
+        precure_name = 'キュアピーチ'
+        prologue = 'ピンクのハートはあいあるしるし！\nもぎたてフレッシュ、キュアピーチ！'
+
+    }
+    miki {
+        name = '蒼乃美希'
+        precure_name = 'キュアベリー'
+        prologue = 'ブルーのハートは希望のしるし！\n摘みたてフレッシュ、キュアベリー！'
+    }
+    inori {
+        name = '山吹祈里'
+        precure_name = 'キュアパイン'
+        prologue = 'イエローハートは祈りのしるし！\nとれたてフレッシュ、キュアパイン！'
+    }
+    setsuna {
+        name = '東せつな'
+        precure_name = 'キュアパッション'
+        prologue = '真っ赤なハートは幸せの証！\n熟れたてフレッシュ、キュアパッション！'
+
+    }
+    tsubomi {
+        name = '花咲つぼみ'
+        precure_name = 'キュアブロッサム'
+        prologue = '大地に咲く一輪の花！キュアブロッサム！'
+    }
+    erika {
+        name = '来海えりか'
+        precure_name = 'キュアマリン'
+        prologue = '海風に揺れる一輪の花！キュアマリン！'
+    }
+    itsuki {
+        name = '明堂院いつき'
+        precure_name = 'キュアサンシャイン'
+        prologue = '陽の光浴びる一輪の花！キュアサンシャイン！'
+    }
+    yuri {
+        name = '月影ゆり'
+        precure_name = 'キュアムーンライト'
+        prologue = '月光に冴える一輪の花！キュアムーンライト！'
+    }
+    hibiki {
+        name = '北条響'
+        precure_name = 'キュアメロディ'
+        prologue = '爪弾くは荒ぶる調べ！キュアメロディ！'
+    }
+    kanade {
+        name = '南野奏'
+        precure_name = 'キュアリズム'
+        prologue = '爪弾くはたおやかな調べ！キュアリズム！'
+    }
+    eren {
+        name = '黒川エレン'
+        precure_name = 'キュアビート'
+        prologue = '爪弾くは魂の調べ！キュアビート！'
+    }
+    ako {
+        name = '調辺アコ'
+        precure_name = 'キュアミューズ'
+        prologue = '爪弾くは女神の調べ！キュアミューズ！'
+
+    }
+    miyuki {
+        name = '星空みゆき'
+        precure_name = 'キュアハッピー'
+        prologue = 'キラキラ輝く未来の光！キュアハッピー！'
+    }
+    akane {
+        name = '日野あかね'
+        precure_name = 'キュアサニー'
+        prologue = '太陽サンサン熱血パワー！キュアサニー！'
+    }
+    yayoi {
+        name = '黄瀬やよい'
+        precure_name = 'キュアピース'
+        prologue = "ピカピカぴかりんじゃんけんポン！(${-> def num = new Random().nextInt(3); num == 0 ? 'グー' : (num == 1 ? 'チョキ' : 'パー')}) キュアピース！"
+    }
+    nao {
+        name = '緑川なお'
+        precure_name = 'キュアマーチ'
+        prologue = '勇気リンリン直球勝負！キュアマーチ！'
+    }
+    reika {
+        name = '青木れいか'
+        precure_name = 'キュアビューティ'
+        prologue = 'しんしんと降り積もる清き心、キュアビューティ！'
+    }
+    mana {
+        name = '相田マナ'
+        precure_name = 'キュアハート'
+        prologue = 'みなぎる愛！キュアハート！'
+    }
+    rikka {
+        name = '菱川六花'
+        precure_name = 'キュアダイヤモンド'
+        prologue = '英知の光！キュアダイヤモンド'
+
+    }
+    arisu {
+        name = '四葉ありす'
+        precure_name = 'キュアロゼッタ'
+        prologue = 'ひだまりポカポカ！キュアロゼッタ！'
+    }
+    makoto {
+        name = '剣崎真琴'
+        precure_name = 'キュアソード'
+        prologue = '勇気の刃！キュアソード！'
+    }
+    aguri {
+        name = '円亜久里'
+        precure_name = 'キュアエース'
+        prologue = '愛の切り札！キュアエース！'
+    }
+    megumi {
+        name = '愛乃めぐみ'
+        precure_name = 'キュアラブリー'
+        prologue = '世界に広がるビッグな愛！キュアラブリー！'
+    }
+    hime {
+        name = '白雪ひめ'
+        precure_name = 'キュアプリンセス'
+        prologue = '天空に舞う蒼き風！キュアプリンセス！'
+    }
+    yuuko {
+        name = '大森ゆうこ'
+        precure_name = 'キュアハニー'
+        prologue = '大地に実る命の光！キュアハニー！'
+    }
+    iona {
+        name = '氷川いおな'
+        precure_name = 'キュアフォーチュン'
+        prologue = '夜空にきらめく希望の星！キュアフォーチュン！'
+    }
+    haruka {
+        name = '春野はるか'
+        precure_name = 'キュアフローラ'
+        prologue = '咲き誇る花のプリンセス！キュアフローラ！'
+    }
+    minami {
+        name = '海藤みなみ'
+        precure_name = 'キュアマーメイド'
+        prologue = '澄みわたる海のプリンセス！キュアマーメイド！'
+    }
+    kirara {
+        name = '天ノ川きらら'
+        precure_name = 'キュアトゥインクル'
+        prologue = 'きらめく星のプリンセス！キュアトゥインクル！'
+    }
+    towa {
+        name = '紅城トワ'
+        precure_name = 'キュアスカーレット'
+        prologue = '真紅の炎のプリンセス！キュアスカーレット！'
+    }
+    mirai {
+        name = '朝比奈みらい'
+        precure_name = 'キュアミラクル'
+        prologue = 'ふたりの奇跡！キュアミラクル'
+    }
+    riko {
+        name = '十六夜リコ'
+        precure_name = 'キュアマジカル'
+        prologue = 'ふたりの魔法！キュアマジカル！'
+    }
+    kotoha {
+        name = '花海ことは'
+        precure_name = 'キュアフェリーチェ'
+        prologue = 'あまねく生命に祝福を！キュアフェリーチェ！'
+    }
+    ichika {
+        name = '宇佐美いちか'
+        precure_name = 'キュアホイップ'
+        prologue = '元気と笑顔を！レッツ・ラ・まぜまぜ！キュアホイップ！できあがり！'
+    }
+    himari {
+        name = '有栖川ひまり'
+        precure_name = 'キュアカスタード'
+        prologue = '知性と勇気を！レッツ・ラ・まぜまぜ！キュアカスタード！できあがり！'
+    }
+    aoi {
+        name = '立神あおい'
+        precure_name = 'キュアジェラード'
+        prologue = '自由と情熱を！レッツ・ラ・まぜまぜ！キュアジェラード！できあがり！'
+    }
+    yukari {
+        name = '琴爪ゆかり'
+        precure_name = 'キュアマカロン'
+        prologue = '美しさとときめきを！レッツ・ラ・まぜまぜ！キュアマカロン！できあがり！'
+    }
+    akira {
+        name = '剣城あきら'
+        precure_name = 'キュアショコラ'
+        prologue = '強さと愛を！レッツ・ラ・まぜまぜ！キュアショコラ！できあがり！'
+    }
+    shieru {
+        name = 'キラ星シエル'
+        precure_name = 'キュアパルフェ'
+        prologue = '夢と希望を！レッツ・ラ・まぜまぜ！キュアパルフェ！できあがり！'
+    }
+    hana {
+        name = '野乃はな'
+        precure_name = 'キュアエール'
+        prologue = 'みんなを応援！元気のプリキュア！キュアエール！'
+    }
+    saaya {
+        name = '薬師寺さあや'
+        precure_name = 'キュアアンジュ'
+        prologue = 'みんなを癒やす！知恵のプリキュア！キュアアンジュ！'
+    }
+    homare {
+        name = '輝木ほまれ'
+        precure_name = 'キュアエトワール'
+        prologue = 'みんな輝け！力のプリキュア！キュアエトワール！'
+    }
+    emiru {
+        name = '愛崎えみる'
+        precure_name = 'キュアマシェリ'
+        prologue = 'みんな大好き！愛のプリキュア！キュアマシェリ！'
+    }
+    amour {
+        name = 'ルールーアムール'
+        precure_name = 'キュアアムール'
+        prologue = 'みんな大好き！愛のプリキュア！キュアアムール！'
+    }
+    hikaru {
+        name = '星奈ひかる'
+        precure_name = 'キュアスター'
+        prologue = '宇宙に輝くキラキラ星！キュアスター！'
+    }
+    rara {
+        name = '羽衣ララ'
+        precure_name = 'キュアミルキー'
+        prologue = '天にあまねくミルキーウェイ！キュアミルキー！'
+    }
+    erena {
+        name = '天宮えれな'
+        precure_name = 'キュアソレイユ'
+        prologue = '宇宙を照らす！灼熱のきらめき！キュアソレイユ！'
+    }
+    madoka {
+        name = '香久矢まどか'
+        precure_name = 'キュアセレーネ'
+        prologue = '夜空に輝く！神秘の月あかり！キュアセレーネ！'
+    }
+    yuni {
+        name = 'ユニ'
+        precure_name = 'キュアコスモ'
+        prologue = '銀河に光る虹色のスペクトル！キュアコスモ！'
+    }
+    nodoka {
+        name = '花寺のどか'
+        precure_name = 'キュアグレース'
+        prologue = '重なる二つの花！キュアグレース！'
+    }
+    chiyu {
+        name = '沢泉ちゆ'
+        precure_name = 'キュアフォンテーヌ'
+        prologue = '交わる二つの流れ、キュアフォンテーヌ！'
+    }
+    hinata {
+        name = '平光ひなた'
+        precure_name = 'キュアスパークル'
+        prologue = '溶け合う二つの光、キュアスパークル！'
+    }
+    asumi {
+        name = '風鈴アスミ'
+        precure_name = 'キュアアース'
+        prologue = '時を経てつながる二つの風、キュアアース！'
+    }
+    manatsu {
+        name = '夏海まなつ'
+        precure_name = 'キュアサマー'
+        prologue = 'ときめく常夏！キュアサマー！'
+    }
+    sango {
+        name = '涼村さんご'
+        precure_name = 'キュアコーラル'
+        prologue = 'きらめく宝石！キュアコーラル！'
+    }
+    minori {
+        name = '一ノ瀬みのり'
+        precure_name = 'キュアパパイア'
+        prologue = 'ひらめく果実！キュアパパイア！'
+    }
+    asuka {
+        name = '滝沢あすか'
+        precure_name = 'キュアフラミンゴ'
+        prologue = 'はためく翼！キュアフラミンゴ！'
+    }
+    laura {
+        name = 'ローラ・アポロドーロス・ヒュギーヌス・ラメール'
+        precure_name = 'キュアラメール'
+        prologue = 'ゆらめく大海原！キュアラメール！'
+    }
+}
+
+series {
+    unmarked {
+        title = 'ふたりはプリキュア'
+        broadcast_from = '2004/2/1'
+        broadcast_to = '2005/1/30'
+        girls = ['nagisa', 'honoka']
+    }
+    maxheart {
+        title = 'ふたりはプリキュア Max Heart'
+        broadcast_from = '2005/2/6'
+        broadcast_to = '2006/1/29'
+        girls = ['nagisa', 'honoka', 'hikari']
+    }
+    splashstar {
+        title = 'ふたりはプリキュア Splash Star'
+        broadcast_from = '2006/2/5'
+        broadcast_to = '2007/1/28'
+        girls = ['saki', 'mai']
+    }
+    yes {
+        title = 'Yes！プリキュア5'
+        broadcast_from = '2007/2/4'
+        broadcast_to = '2008/1/27'
+        girls = ['nozomi', 'rin', 'urara', 'komachi', 'karen']
+    }
+    gogo {
+        title = 'Yes！プリキュア5 Go Go！'
+        broadcast_from = '2008/2/3'
+        broadcast_to = '2009/1/25'
+        girls = ['nozomi', 'rin', 'urara', 'komachi', 'karen', 'kurumi']
+    }
+    fresh {
+        title = 'フレッシュプリキュア'
+        broadcast_from = '2009/2/1'
+        broadcast_to = '2010/1/31'
+        girls = ['love', 'miki', 'inori', 'setsuna']
+    }
+    heartcatch {
+        title = 'ハートキャッチプリキュア'
+        broadcast_from = '2010/2/7'
+        broadcast_to = '2011/1/30'
+        girls = ['tsubomi', 'erika', 'itsuki', 'yuri']
+    }
+    suite {
+        title = 'スイートプリキュア'
+        broadcast_from = '2011/2/6'
+        broadcast_to = '2012/2/29'
+        girls = ['hibiki', 'kanade', 'eren', 'ako']
+    }
+    smile {
+        title = 'スマイルプリキュア'
+        broadcast_from = '2012/2/5'
+        broadcast_to = '2013/1/27'
+        girls = ['miyuki', 'akane', 'yayoi', 'nao', 'reika']
+    }
+    dokidoki {
+        title = 'ドキドキ！プリキュア'
+        broadcast_from = '2013/2/3'
+        broadcast_to = '2014/1/26'
+        girls = ['mana', 'rikka', 'arisu', 'makoto', 'aguri']
+    }
+    happinesscharge {
+        title = 'ハピネスチャージプリキュア'
+        broadcast_from = '2014/2/2'
+        broadcast_to = '2015/1/25'
+        girls = ['megumi', 'hime', 'yuuko', 'iona']
+    }
+    goprincess {
+        title = 'Go！プリンセスプリキュア'
+        broadcast_from = '2015/2/1'
+        broadcast_to = '2016/1/31'
+        girls = ['haruka', 'minami', 'kirara', 'towa']
+    }
+    mahotsukai {
+        title = '魔法使いプリキュア'
+        broadcast_from = '2016/2/7'
+        broadcast_to = '2017/1/29'
+        girls = ['mirai', 'riko', 'kotoha']
+    }
+    kirakira {
+        title = 'キラキラ☆プリキュアアラモード'
+        broadcast_from = '2017/2/5'
+        broadcast_to = '2018/1/28'
+        girls = ['ichika', 'himari', 'aoi', 'yukari', 'akira', 'shieru']
+    }
+    hugtto {
+        title = 'HUGっと！プリキュア'
+        broadcast_from = '2018/2/4'
+        broadcast_to = '2019/1/27'
+        girls = ['hana', 'saaya', 'homare', 'emiru', 'amour']
+    }
+    startwinkle {
+        title = 'スター☆トゥインクルプリキュア'
+        broadcast_from = '2019/2/3'
+        broadcast_to = '2020/01/26'
+        girls = ['hikaru', 'rara', 'erena', 'madoka', 'yuni']
+    }
+    healingood {
+        title = 'ヒーリングっど♥プリキュア'
+        broadcast_from = '2020/2/2'
+        broadcast_to = '2021/2/21'
+        girls = ['nodoka', 'chiyu', 'hinata', 'asumi']
+    }
+    tropicalrouge {
+        title = 'トロピカル～ジュ！プリキュア'
+        broadcast_from = '2021/2/28'
+        girls = ['manatsu', 'sango', 'minori', 'asuka', 'laura']
+    }
+}
